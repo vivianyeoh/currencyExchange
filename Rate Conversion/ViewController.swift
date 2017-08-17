45d54b9ab2a82970dd3b0e81b3c0db62cf9a07d3
Vivian <vivian.yeoh@travelconnector.com>
2017-08-17 21:27:30 +0800

    Initial Commit

---
+++ Rate Conversion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rate Conversion
+//
+//  Created by Vivian on 17/08/2017.
+//  Copyright © 2017 Vivian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
